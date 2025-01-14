75e0c51bc6cc3bbd71f7c22821d173695e7466e3
Grégoire Chapeaux <gregoire.chapeaux@apexo.net>
2025-01-14 17:30:36 -0500

    Init project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MRE_ExpanderCV
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MRE_ExpanderCV
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CV.ItemsSource = Enumerable.Range(1,25);
+        }
+
+        private void Expander_ExpandedChanged(object sender, CommunityToolkit.Maui.Core.ExpandedChangedEventArgs e)
+        {
+            if (sender is Expander ex && e.IsExpanded)
+            {
+                Console.WriteLine($"=== Expander {ex.BindingContext} clicked. ===");
+                List<Expander> expanders = CV.GetVisualTreeDescendants().OfType<Expander>().ToList();
+                foreach (var expander in expanders)
+                {
+                    if (expander != ex)
+                    {
+                        expander.IsExpanded = false;
+                    }
+                }
+
+                int position = expanders.IndexOf(ex);
+                CV.ScrollTo(position, position: ScrollToPosition.Start);
+
+            }
+        }
+
+    }
+
+}
